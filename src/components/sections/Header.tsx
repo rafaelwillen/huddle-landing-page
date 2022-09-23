9860c1bc182cf6e97f82daef2f578dbc9aa33115
Rafael Willen <rafaelpadre20@gmail.com>
2022-09-23 16:26:38 +0100

    Adjusted the size of the elements

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
         </div>
         <section className="mt-10 px-3 lg:flex items-center">
           <div className="flex flex-col gap-6 flex-[2] lg:gap-8">
-            <h1 className="text-center text-xl font-semibold font-heading lg:text-left">
+            <h1 className="text-center text-xl font-semibold font-heading lg:text-left lg:text-3xl lg:leading-relaxed">
               Build the Community <br />
               Your Fans Will Love
             </h1>
@@ -32,7 +32,7 @@ const Header = () => {
             </p>
             <a
               href="#"
-              className="bg-primary w-full block text-center text-white text-sm py-2 rounded-full shadow-lg lg:w-1/2 lg:py-3"
+              className="bg-primary w-full block text-center text-white text-sm py-2 rounded-full shadow-lg lg:w-1/2 lg:py-3 lg:font-bold"
             >
               Get Started For Free
             </a>
@@ -40,7 +40,7 @@ const Header = () => {
           <img
             src={mockups}
             width={296}
-            className="w-full p-3 mt-6 lg:mt-0 lg:flex-[3] lg:w-0 lg:max-w-xl"
+            className="w-full p-3 mt-6 lg:mt-0 lg:flex-[3] lg:w-0 lg:max-w-2xl"
             alt="Mockups"
           />
         </section>
