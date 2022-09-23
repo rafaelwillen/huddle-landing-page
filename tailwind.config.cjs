ea8db40bd6e141095c8332025f23ccd722d58941
Rafael Willen <rafaelpadre20@gmail.com>
2022-09-23 13:31:57 +0100

    Add custom theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: "hsl(322, 100%, 66%)",
+      paleCyan: "hsl(193,100%, 96%)",
+      darkCyan: "hsl(192,100%, 9%)",
+      blue: "hsl(208, 11%, 55%)",
+      black: "#000",
+      white: "#fff"
+    },
+    fontFamily: {
+      heading: "Poppins, sans-serif",
+      main: "Open Sans, sans-serif"
+    }
   },
   plugins: [],
 }
